baa299b56b31733adf9c42749b3094c40a80ef5a
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-01-10 13:09:33 -0800

    change 'spots remaining' message depending on how many spots are remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+
+  console.log("spotslog", props.spots);
+  // const [spotsRemainingMessage, setSpotsRemainingMessage] = useState(`${props.spots} spots remaining`)
+
+  const formatSpots = function (spots) {
+    let spotsRemainingMessage = `${spots} spots remaining`
+    
+    if (spots === 0) {
+      spotsRemainingMessage = "no spots remaining";
+    } else if (spots === 1) {
+      spotsRemainingMessage = "1 spot remaining";
+    } 
+    return spotsRemainingMessage;
+  }
 
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
@@ -14,7 +28,7 @@ export default function DayListItem(props) {
     onClick={() => props.setDay(props.name)} 
     className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
