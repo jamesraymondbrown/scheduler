b8e8348e1a72cd850f5ee44cfb88bbd00758020f
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-01-12 14:30:15 -0800

    use api call to update Days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import axios from 'axios';
+
 
 const appointments = {
   "1": {
@@ -44,25 +46,17 @@ const appointments = {
 
 export default function Application(props) {
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
 
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+    .then((response) => {
+      console.log('response', response)
+      setDays(response.data);
+    });
+  }, [])
 
 
   return (
