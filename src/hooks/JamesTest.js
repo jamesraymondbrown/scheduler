cc0d16a48c941213ffa8ee89c76b5c394e55ae5d
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-01-13 07:47:18 -0800

    made custom useVisualMode hook, and tests for that hook

---
+++ src/hooks/JamesTest.js
@@ -0,0 +1,3 @@
+// const array = [0, 1, 2]
+
+// console.log(array[array.length-2]);
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = function(arg, replace = false) {
+    setMode(arg)
+    const newHistory = [...history];
+    if (replace != false) {
+      newHistory.pop();
+      newHistory.push(arg)
+    } else {
+      newHistory.push(arg);
+    }
+    setHistory(newHistory);
+  };
+
+  const back = function() {
+    if (history.length < 2) {
+      setHistory("Cannot go back further")
+      return;
+    }
+    setMode(history[history.length-2])
+    const newHistory = [...history];
+    newHistory.pop();
+    setHistory(newHistory);
+
+  };
+
+
+  return { mode, transition, back }
+};
