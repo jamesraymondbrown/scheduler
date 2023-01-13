f2b842427b637f0b4f64f998273d4812127fb6e7
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-01-12 17:27:01 -0800

    add getInterview helper

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -11,27 +11,37 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  
-
 
   const setDay = day => setState({ ...state, day });
-
-  // const setDays = (days) => {
-  //   setState(prev => ({ ...prev, days }));
-  // }
 
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/appointments")
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, [])
+
+  const schedule = Object.values(dailyAppointments).map((appointment) => {
+
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment 
+        key={appointment.id} 
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />)
+    }
+  );
 
   return (
     <main className="layout">
@@ -52,14 +62,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(dailyAppointments).map((appointment) => {
-          return (
-            <Appointment 
-              key={appointment.id} 
-              {...appointment}
-            />)
-          }
-        )}
+        {schedule}
       </section>
     </main>
   );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,3 +18,20 @@ export function getAppointmentsForDay(state, day) {
 
   return results;
 };
+
+export function getInterview(state, interview) {
+  const results = {};
+
+  if (interview === null) {
+    return null;
+  };
+
+  results.student = interview.student
+  results.interviewer = {
+    id: interview.interviewer,
+    name: state.interviewers[interview.interviewer].name,
+    avatar: state.interviewers[interview.interviewer].avatar
+  };
+
+  return results;
+};
