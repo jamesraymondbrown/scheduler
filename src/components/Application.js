4dd87119adc5af4b1c07ff6be7ef81fb1e8cc2ee
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-01-16 18:35:58 -0800

    moved all Application.js state-related functions to useApplicationData

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,62 +4,18 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {},
-    interviewers: {}
-  });
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    });
-  }, [])
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
-    .then((res) => {
-      console.log('response', res);
-      setState({
-        ...state,
-        appointments
-      });
-    }) 
-  }
-
-  function cancelInterview(id) {
-
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, {interview: null})
-    .then((res) => {
-      //console.log('response', res);
-      // setState({
-      //   ...state,
-      //   appointments
-      // });
-    }) 
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-
-  const setDay = day => setState({ ...state, day });
 
   const schedule = Object.values(dailyAppointments).map((appointment) => {
 
